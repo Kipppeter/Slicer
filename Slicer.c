dfcf126eec88d0e7248c80a899a981d8b6403db4
Peter Kipp <115092312+Kipppeter@users.noreply.github.com>
2022-12-20 02:28:48 +0200

    Add files via upload

--- Slicer.c
+++ Slicer.c
@@ -3,9 +3,13 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 // General size limits
-#define MAXTRIANGLES 100
+#define MAXTRIANGLES 2682
+#define MAXLAYERS 1
+#define MAXPOINTSPERLAYER 10
+#define MAXERROR 0.2
 
 #define MINSIDELENGTH 2
 #define EXTRUSIONRADIUS 0.2
@@ -14,8 +18,7 @@
 #define INPUTDIGIT 0
 #define INPUTCHAR 1
 
-struct layer {
-		int layerNum;
+struct tess {
 		double nX;
 		double nY;
 		double nZ;
@@ -30,14 +33,24 @@ struct layer {
 		double v3Z;
 	} triangle[MAXTRIANGLES];
 	
+struct layer {
+	double x;
+	double y;
+	} l[MAXPOINTSPERLAYER][MAXLAYERS];
+	
 int InputType(char input[]);
 void ReadInput(void);
 void LineEq(double vector[], double point1[], double point2[]);
+void FlatLayer(double height, int layerNum);
+void Dump(void);
 
 int main(void)
 {
+	double begin = clock();
 	ReadInput();
-
+	Dump();
+	double end = clock();
+	printf("Start time: %f\nEnd time: %f\nTotal time: %f", begin, end, (end - begin)/CLOCKS_PER_SEC);
 	return 0;
 }
 
@@ -79,9 +92,19 @@ void ReadInput(void)
 	double val;
 	int i = 0;
 	int current = 0;
+	
+	FILE *fInput;
+	
+	fInput = fopen("sample.stl", "r");
+	printf("FILE OPENED!\n");
 	while(input != '\0')
 	{
-		scanf("%s", input);
+		printf("Reading value %d\n", i);
+		if(current == MAXTRIANGLES)
+		{
+			break;
+		}
+		fscanf(fInput, "%s", input);
 		iT = InputType(input);
 		if(iT == INPUTDIGIT)
 		{
@@ -130,6 +153,9 @@ void ReadInput(void)
 			i++;
 		}
 	}
+	printf("WHILE LOOP DONE!\n");
+	fclose(fInput);
+	printf("FILE CLOSED!\n");
 }
 
 void LineEq(double vector[], double point1[], double point2[])
@@ -137,4 +163,90 @@ void LineEq(double vector[], double point1[], double point2[])
 	vector[0] = (point2[0] - point1[0]) / (point2[2] - point1[2]);
 	vector[1] = (point2[1] - point1[1]) / (point2[2] - point1[2]);
 	vector[2] = 1;
+}
+
+void FlatLayer(double height, int layerNum)
+{
+	int i;
+	int j = 0;
+	float layerHeight = height - (MAXERROR/2);
+	int intersectingTriangles[MAXTRIANGLES] = {-1};
+	
+	for(i = 0; i < MAXTRIANGLES; i++)
+	{
+		if(triangle[i].v1Z <= layerHeight)
+		{
+			intersectingTriangles[j] = i;
+			j++;
+		}
+		else if(triangle[i].v2Z <= layerHeight)
+		{
+			intersectingTriangles[j] = i;
+			j++;
+		}
+		else if(triangle[i].v3Z <= layerHeight)
+		{
+			intersectingTriangles[j] = i;
+			j++;
+		}
+	}
+	
+}
+
+void Dump(void)
+{
+	int i, j;
+	
+	FILE *fOutput;
+	
+	fOutput = fopen("output.txt", "w");
+	
+	for(i = 0; i < MAXTRIANGLES; i++)
+	{
+		fprintf(fOutput, "Triangle %d\n", i);
+		for(j = 0; j < 12; j++)
+		{
+			switch(j)
+			{
+				case 0:
+					fprintf(fOutput, "nX: %f\n", triangle[i].nX);
+					break;
+				case 1:
+					fprintf(fOutput, "nY: %f\n", triangle[i].nY);
+					break;
+				case 2:
+					fprintf(fOutput, "nZ: %f\n", triangle[i].nZ);
+					break;
+				case 3:
+					fprintf(fOutput, "v1X: %f\n", triangle[i].v1X);
+					break;
+				case 4:
+					fprintf(fOutput, "v1Y: %f\n", triangle[i].v1Y);
+					break;
+				case 5:
+					fprintf(fOutput, "v1Z: %f\n", triangle[i].v1Z);
+					break;
+				case 6:
+					fprintf(fOutput, "v2X: %f\n", triangle[i].v2X);
+					break;
+				case 7:
+					fprintf(fOutput, "v2Y: %f\n", triangle[i].v2Y);
+					break;
+				case 8:
+					fprintf(fOutput, "v2Z: %f\n", triangle[i].v2Z);
+					break;
+				case 9:
+					fprintf(fOutput, "v3X: %f\n", triangle[i].v3X);
+					break;
+				case 10:
+					fprintf(fOutput, "v3Y: %f\n", triangle[i].v3Y);
+					break;
+				case 11:
+					fprintf(fOutput, "v3Z: %f\n", triangle[i].v3Z);
+					break;
+			}
+		}
+		fprintf(fOutput, "\n");
+	}
+	fclose(fOutput);
 }
